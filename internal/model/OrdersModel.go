2c8a7ccd1acdc930cc673729bf31624ffe5f9d8d
Andrey Guschin <andrey67895@gmail.com>
2024-07-08 16:42:31 +0500

    АПИ метод - Получение списка загруженных номеров заказов

--- internal/model/OrdersModel.go
+++ internal/model/OrdersModel.go
@@ -3,8 +3,8 @@ package model
 import "time"
 
 type OrdersModel struct {
-	OrdersID   *int64 `json:"number"`
-	Login      *string
+	OrdersID   *int64     `json:"number"`
+	Login      *string    `json:"-"`
 	Accrual    *int64     `json:"accrual"`
 	Status     *string    `json:"status"`
 	UploadedAT *time.Time `json:"uploaded_at"`
