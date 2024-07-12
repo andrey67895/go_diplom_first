98ed3138387c77b4417b268af84569e5ba821ec6
Andrey Guschin <andrey67895@gmail.com>
2024-07-12 17:57:46 +0500

    АПИ метод - Получение списка загруженных номеров заказов. Fix bug

--- internal/config/Config.go
+++ internal/config/Config.go
@@ -12,7 +12,7 @@ var AccrualSystemAddress string
 
 func InitServerConfig() {
 	flag.StringVar(&DatabaseDsn, "d", fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", `localhost`, 5434, `docker`, `docker`, `postgres`), "Aдрес подключения к базе данных")
-	flag.StringVar(&RunAddress, "a", ":8787", "Aдрес и порт запуска сервиса")
+	flag.StringVar(&RunAddress, "a", ":8080", "Aдрес и порт запуска сервиса")
 	flag.StringVar(&AccrualSystemAddress, "r", "", "адрес системы расчёта начислений")
 
 	flag.Parse()
