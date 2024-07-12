db0881577036cb013e82ca82732066b0cce4f31d
Andrey Guschin <andrey67895@gmail.com>
2024-07-12 17:54:16 +0500

    АПИ метод - Получение списка загруженных номеров заказов. Fix bug

--- internal/app/App.go
+++ internal/app/App.go
@@ -3,6 +3,7 @@ package app
 import (
 	"context"
 	"net/http"
+	"os/exec"
 
 	"github.com/andrey67895/go_diplom_first/internal/config"
 	"github.com/andrey67895/go_diplom_first/internal/database"
@@ -14,5 +15,14 @@ func InitServer() {
 	helpers.InitLog()
 	config.InitServerConfig()
 	database.InitDB(context.Background())
+	go func() {
+		cmd := exec.Command("cmd", "/C", "start", config.AccrualSystemAddress)
+		err := cmd.Start()
+		if err != nil {
+			helpers.TLog.Fatal(err.Error())
+			return
+		}
+	}()
 	helpers.TLog.Fatal(http.ListenAndServe(config.RunAddress, transport.GetRouters()))
+
 }
