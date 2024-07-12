7a05486d7089cd30a310a0105da11b804ab335a7
Andrey Guschin <andrey67895@gmail.com>
2024-07-13 00:07:02 +0500

    АПИ метод - Получение текущего баланса пользователя. Fix bugs

--- internal/database/DBStorage.go
+++ internal/database/DBStorage.go
@@ -106,7 +106,7 @@ func (db DBStorageModel) CreateOrders(ordersModel model.OrdersModel) error {
 func (db DBStorageModel) UpdateOrders(ordersAccrualModel model.OrdersAccrualModel, itProcessed bool, login string) error {
 	var err error
 	if itProcessed {
-		_, err = db.DB.ExecContext(db.ctx, `UPDATE orders SET accrual=$1,status=$2 WHERE orders_id=$3; INSERT INTO orders(orders_id, login) values ($3,$4);`, ordersAccrualModel.Accrual, ordersAccrualModel.Status, ordersAccrualModel.OrderID, login)
+		_, err = db.DB.ExecContext(db.ctx, `UPDATE orders SET accrual=$1,status=$2 WHERE orders_id=$3; INSERT INTO current_balance as ca (login, current) values ($4,$1) on conflict (login) do update set current = (EXCLUDED.current  + ca."current");`, ordersAccrualModel.Accrual, ordersAccrualModel.Status, ordersAccrualModel.OrderID, login)
 	} else {
 		_, err = db.DB.ExecContext(db.ctx, `UPDATE orders SET accrual=$1,status=$2 WHERE orders_id=$3`, ordersAccrualModel.Accrual, ordersAccrualModel.Status, ordersAccrualModel.OrderID)
 	}
