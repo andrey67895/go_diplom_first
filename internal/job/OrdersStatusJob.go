6fe07920cbac321bb4ce5e8d8024d8e9fa25c653
Andrey Guschin <andrey67895@gmail.com>
2024-07-16 14:30:35 +0500

    Ticker add for job

--- internal/job/OrdersStatusJob.go
+++ internal/job/OrdersStatusJob.go
@@ -3,6 +3,7 @@ package job
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/andrey67895/go_diplom_first/internal/config"
@@ -13,7 +14,7 @@ import (
 
 var client http.Client
 
-func GerAndUpdateOrderStatusByAccrual(login string, number string) {
+func GetAndUpdateOrderStatusByAccrual(login string, number string) (*http.Response, error) {
 	url := config.AccrualSystemAddress + "/api/orders/" + number
 	r, _ := http.NewRequest(http.MethodGet, url, nil)
 	body, err := client.Do(r)
@@ -26,31 +27,42 @@ func GerAndUpdateOrderStatusByAccrual(login string, number string) {
 		err = json.NewDecoder(body.Body).Decode(&tModel)
 		if err != nil {
 			helpers.TLog.Error(err.Error())
-			return
+			return body, err
 		}
 		if *tModel.Status == "PROCESSING" || *tModel.Status == "INVALID" || *tModel.Status == "PROCESSED" {
 			err := database.DBStorage.UpdateOrders(tModel, *tModel.Status == "PROCESSED", login)
 			if err != nil {
 				helpers.TLog.Error(err.Error())
-				return
+				return body, err
 			}
 		}
 	}
+	return body, nil
 }
 
 func OrdersStatusJob() {
-	for {
+	second := 1
+	ticker := time.NewTicker(time.Duration(second) * time.Second)
+	for range ticker.C {
 		helpers.TLog.Info("Job: Запуск проверки статусов")
 		orders, err := database.DBStorage.GetOrdersByNotFinalStatus()
 		if err != nil {
 			helpers.TLog.Error(err.Error())
 			return
 		}
+		var body *http.Response
 		for _, order := range *orders {
-			GerAndUpdateOrderStatusByAccrual(*order.Login, *order.OrdersID)
+			body, _ = GetAndUpdateOrderStatusByAccrual(*order.Login, *order.OrdersID)
+			if body.StatusCode == 429 {
+				i, err := strconv.Atoi(body.Header.Get("Retry-After"))
+				if err != nil {
+					helpers.TLog.Error(err.Error())
+				}
+				ticker.Reset(time.Duration(i) * time.Second)
+			} else {
+				ticker.Reset(time.Duration(second) * time.Second)
+			}
 		}
 		helpers.TLog.Info("Job: Окончание проверки статусов")
-		time.Sleep(3 * time.Second)
 	}
-
 }
